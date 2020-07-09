d28e0726f982d3499bd342db0e536edd6b0ece24
Kuba <jciesmail@gmail.com>
2020-07-09 11:57:57 +0200

    Add commander

---
+++ Commander/Commander/Profiles/CommandsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
